9f951a6337b3c4fdea0fd9af014441289d539a72
DevRP <l01022899266@gmail.com>
2024-04-27 02:34:53 +0900

    profile 제작

---
+++ src/lib/Projects.ts
@@ -0,0 +1,30 @@
+const projects = [
+	{
+		title: 'Tanks!',
+		technologies: ['Unity', 'C#'],
+		description:
+			'Top down tank shooting game made with Unity.',
+		url: 'https://github.com/hun1001/Project_RF'
+	},
+	{
+		title: 'GGMALLSTAR',
+		technologies: ['Unity', 'C#'],
+		description:
+			'2D Fighting game made with Unity.',
+		url: 'https://github.com/KanghyuckMoon/GGMALLSTAR'
+	},
+	{
+		title: 'Popping Buba',
+		technologies: ['Unity', 'C#'],
+		description: 'Casual game develop in Game Jam for 3 days',
+		url: 'https://github.com/White-Palette/PoppingBuba'
+	},
+	{
+		title: 'William Shakespeare',
+		technologies: ['Unity', 'C#'],
+		description: 'Story game develop in Game Jam for 3 days',
+		url: 'https://github.com/rtq9731/Linear'
+	}
+];
+
+export default projects;
---
+++ src/lib/Skills.ts
@@ -0,0 +1,8 @@
+const skills = {
+	programming: ['C', 'C#', 'C++', 'Python', 'JavaScript', 'TypeScript', 'Node.js', 'Svelte.js', 'HTML', 'CSS'],
+	databases: ['MS-SQL', 'Redis'],
+	tools: ['Visual Studio', 'VS Code', 'Unity', 'Git', 'Jenkins'],
+	languages: ['Korean', 'English'],
+};
+
+export default skills;
