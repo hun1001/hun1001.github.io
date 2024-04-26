805861611ab827931365935d20b54d8c0e1675e3
DevRP <l01022899266@gmail.com>
2024-04-27 02:33:30 +0900

    프로필 참고하여 제작 완료 lib 추가 및 about project home에 기본 정보 추가함

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Profile and Blog
+# hun1001's Profile
+[Refference](https://github.com/Ladvace/SvelteKit-Portfolio)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,113 @@
-<h1>hun1001 Profile</h1>
-<p>
-    Visit <a href="https://github.com/hun1001">github.com/hun1001</a>
-</p>
+<script>
+    import FaLinkedin from "svelte-icons/fa/FaLinkedin.svelte";
+    import FaGithub from "svelte-icons/fa/FaGithub.svelte";
+    import FaEnvelope from "svelte-icons/fa/FaEnvelope.svelte";
+    import { modalOpened } from "$lib/store";
+</script>
+
+<svelte:head>
+    <title>hun1001 - Main page</title>
+</svelte:head>
+
+<main>
+    <h1>Hi!✋ <br /> I'm RpDev a Server Developer 💻 based in Korean</h1>
+    <div class="icons">
+        <div
+            role="button"
+            tabindex="0"
+            on:keypress={() => {
+                modalOpened.set(true);
+            }}
+            on:click={() => {
+                modalOpened.set(true);
+            }}
+        >
+            <div class="icon">
+                <FaEnvelope />
+            </div>
+        </div>
+
+        <a
+            href="https://github.com/hun1001"
+            aria-label="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <div class="icon">
+                <FaGithub />
+            </div>
+        </a>
+        <a
+            href="https://www.linkedin.com/in/%ED%83%9C%ED%9B%88-%EC%9E%84-b0a857306/"
+            aria-label="Linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <div class="icon">
+                <FaLinkedin />
+            </div>
+        </a>
+    </div>
+</main>
+
+<style>
+    a {
+        color: white;
+        text-decoration: none;
+    }
+    main {
+        text-align: center;
+        padding: 0;
+        margin: 0 auto;
+        text-align: center;
+
+        display: flex;
+        flex-direction: column;
+        height: calc(100vh - 80px - 88px);
+        justify-content: center;
+        align-items: center;
+    }
+
+    h1 {
+        font-weight: 700;
+    }
+
+    main > h1 {
+        margin: 50px 10px 0;
+        font-size: 36px;
+    }
+
+    .icons {
+        display: flex !important;
+        justify-content: center !important;
+        align-items: center;
+        gap: 20px;
+        cursor: pointer;
+        font-size: 30px;
+        display: flex;
+        justify-content: space-between;
+        max-width: 200px;
+        margin: 50px auto 0;
+    }
+
+    .icon {
+        cursor: pointer;
+        transition: color 0.2s ease-in-out;
+        width: 40px;
+    }
+    .icon:hover {
+        color: #ca3c25;
+    }
+
+    @media (min-width: 900px) {
+        main > h1 {
+            font-size: 48px;
+        }
+    }
+
+    @media (min-width: 600px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
